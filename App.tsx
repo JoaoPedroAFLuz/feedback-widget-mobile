df95fede89eaddc74242b68c9c1d637c44119da9
João Pedro Luz <joao.pedro.luz@hotmail.com>
2022-05-05 20:49:16 -0300

    feat: Created Widget's feedback types

--- App.tsx
+++ App.tsx
@@ -1,17 +1,13 @@
 /* eslint-disable camelcase */
-import 'react-native-gesture-handler';
-
+import {
+  Inter_400Regular,
+  Inter_500Medium, useFonts,
+} from '@expo-google-fonts/inter';
+import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
-import AppLoading from 'expo-app-loading';
-
-import {
-  useFonts,
-  Inter_400Regular,
-  Inter_500Medium,
-} from '@expo-google-fonts/inter';
-
-import { Widget } from './src/components/Widget';
+import 'react-native-gesture-handler';
+import Widget from './src/components/Widget';
 import { theme } from './src/theme';
 
 export default function App() {
