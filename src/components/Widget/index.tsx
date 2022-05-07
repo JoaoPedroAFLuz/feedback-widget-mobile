69d3ee85943cff39233ae94e3aa8f0db28a6e01e
João Pedro Luz <joao.pedro.luz@hotmail.com>
2022-05-06 22:49:16 -0300

    feat: Created Widget's feedback content

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -4,8 +4,11 @@ import { useRef } from 'react';
 import { TouchableOpacity } from 'react-native';
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 import { theme } from '../../theme';
-import { Options } from '../Options';
+import { feedbackTypes } from '../../utils/feedbackTypes';
+import { Form } from '../Form';
 import { styles } from './styles';
+
+export type FeedbackType = keyof typeof feedbackTypes;
 
 function Widget() {
   const bottomSheetRef = useRef<BottomSheet>(null);
@@ -34,7 +37,9 @@ function Widget() {
         backgroundStyle={styles.modal}
         handleIndicatorStyle={styles.indicator}
       >
-        <Options />
+        <Form
+          feedbackType="BUG"
+        />
       </BottomSheet>
 
     </>
